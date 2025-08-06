3e43b7d13e31ac6c54e820f70b444faf2ff53e02
madhushan-git <madhushandharmasena@gmail.com>
2025-08-06 16:49:30 +0530

    Structure created

---
+++ templates.sh
@@ -0,0 +1,36 @@
+# Make drirectory
+mkdir -p src/components
+mkdir -p src/entity
+mkdir -p src/constants
+mkdir -p src/exceptions
+mkdir -p src/loggings
+mkdir -p src/pipeline
+mkdir -p src/clouds
+mkdir -p src/utils
+
+mkdir -p Network_Data
+
+mkdir -p .github/workflows
+
+mkdir -p Notebooks
+
+# Creating files
+touch src/__init__.py
+touch src/components/__init__.py
+touch src/entity/__init__.py
+touch src/constants/__init__.py
+touch src/exceptions/__init__.py
+touch src/loggings/__init__.py
+touch src/pipeline/__init__.py
+touch src/clouds/__init__.py
+touch src/utils/__init__.py
+
+touch .github/workflows/main.yml
+
+touch Dockerfile
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+
+echo "Directry and files created successfuly"
